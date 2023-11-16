127e5cc5ad6c81c3bbebac0d5c804df91b5b962b
PeykoGeorgiev <peyko92gm@gmail.com>
2023-11-16 20:25:22 +0100

    Create Catalog page

---
+++ client/src/components/catalog/Catalog.jsx
@@ -0,0 +1,217 @@
+import { Link } from 'react-router-dom'
+
+import '../../../public/styles/30_pages/catalog.css'
+
+
+export default function Catalog () {
+    return (
+        <section className='cata'>
+            <h1>All Stories</h1>
+            {/* <!-- Display div: with information about every game (if any) --> */}
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            <div class="allStories">
+                <div class="allStories-info">
+                    <img src="./images/avatar-1.jpg" />
+                    <h6>Action</h6>
+                    <h2>Zombie lang</h2>
+                    <Link to='/details'>Details</Link>
+                </div>
+
+            </div>
+
+            
+
+{/*         <!-- Display paragraph: If there is no games  --> */}
+            <h3 class="no-stories">No articles yet</h3>
+        </section>
+    )
+}
