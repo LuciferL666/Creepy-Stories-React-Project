19bb4ce94c147fdf825f186b9b0b35aea75a2221
PeykoGeorgiev <peyko92gm@gmail.com>
2023-11-26 22:56:34 +0100

    change login to controlled form

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,19 +1,44 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import useForm from "../../hooks/useForm";
 
-import '../../../public/styles/30_pages/login.css'
+import "../../../public/styles/30_pages/login.css";
 
-export default function Login () {
-    return (
+export default function Login() {
+  const {values, onChange, onSubmit} = useForm({
+    email: '',  
+    password: '',
+  })
 
-        <div className="log">
+  return (
+    <div className="log">
       <h1>Login</h1>
       <h2>Get in and start writing</h2>
-<form action="" id="log">
-  <input type="email" class="email-log" placeholder="Your Email" required/>
-  <input type="password" class="password-login" placeholder="Password" required/>
-  <button className="sub-button" type="submit">Submit</button>
-  <Link to="/login"><button type="button" class="goToRegistration">Don't have an account</button></Link>
-</form>
+      <form id="log" onSubmit={onSubmit}>
+        <input
+          type="email"
+          class="email-log"
+          placeholder="Your Email"
+          required
+          onChange={onChange}
+          value={values.email}
+        />
+        <input
+          type="password"
+          class="password-login"
+          placeholder="Password"
+          required
+          onChange={onChange}
+          value={values.password}
+        />
+        <button className="sub-button" type="submit">
+          Submit
+        </button>
+        <Link to="/login">
+          <button type="button" class="goToRegistration">
+            Don't have an account
+          </button>
+        </Link>
+      </form>
     </div>
-    )
+  );
 }
