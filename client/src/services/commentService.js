ce5b9d7f5b5cbf7032e6b6fc25327a6e45a525e9
PeykoGeorgiev <peyko92gm@gmail.com>
2023-11-26 22:57:14 +0100

    update comments

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -3,7 +3,10 @@ import * as request from '../lib/request';
 const baseUrl = 'http://localhost:3030/jsonstore/comments';
 
 export const getAll = async (storyId) => {
-    const result = await request.get(baseUrl);
+    const query = new URLSearchParams({
+        where: `storyId="${storyId}"`
+    })
+    const result = await request.get(`${baseUrl}`);
 
     return Object.values(result).filter(comment => comment.storyId === storyId);
 };
