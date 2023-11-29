6ff441c1c41ee9ae1257fa6fa973d780392d067b
PeykoGeorgiev <peyko92gm@gmail.com>
2023-11-29 16:53:12 +0100

    Change Register form to controlled

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,8 +30,13 @@ function App() {
    navigate(Path.Home)
   };
 
+  const registerSubmitHandler = async (values) => {
+    console.log(values);
+  }
+
   const values = {
     loginSubmitHandler,
+    registerSubmitHandler,
     username: auth.username,
     email: auth.email,
     isAuthenticated: !!auth.username,
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,23 +1,76 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useContext } from "react";
 
-import '../../../public/styles/30_pages/register.css'
+import AuthContext from "../../contexts/authContext";
+import useForm from "../../hooks/useForm";
 
+import "../../../public/styles/30_pages/register.css";
 
-export default function Register () {
- return (
+const RegisterFormKeys = {
+  Name: "name",
+  Email: "email",
+  Password: "password",
+  PasswordConfirm: "repeat-password",
+};
+
+export default function Register() {
+  const { registerSubmitHandler } = useContext(AuthContext);
+  const { values, onChange, onSubmit } = useForm(registerSubmitHandler, {
+    [RegisterFormKeys.Name]: "",
+    [RegisterFormKeys.Email]: "",
+    [RegisterFormKeys.Password]: "",
+    [RegisterFormKeys.PasswordConfirm]: "",
+  });
+
+  return (
     <div className="reg">
       <h1>Sign Up</h1>
       <h2>You want to be able to write then sign up now</h2>
-<form action="" id="sign">
-  <input type="name" className="name-reg" placeholder="Your Name" required/>
-  <input type="email" class="email-reg" placeholder="Your Email" required/>
-  <input type="password" class="password-reg" placeholder="Password" required/>
-  <input type="password" class="rPassword-reg" placeholder="Repeat Password" required/>
-  <button className="sub-button" type="submit">Submit</button>
-  <Link to="/login"><button type="button" class="goToLogin">Have an account</button></Link>
-  
-</form>
+      <form id="sign" onSubmit={onSubmit}>
+        <input
+          type="name"
+          className="name-reg"
+          placeholder="Your Name"
+          required
+          onChange={onChange}
+          values={values[RegisterFormKeys.Name]}
+        />
+        <input
+          type="email"
+          class="email-reg"
+          name="email"
+          placeholder="Your Email"
+          required
+          onChange={onChange}
+          values={values[RegisterFormKeys.Email]}
+        />
+        <input
+          type="password"
+          class="password-reg"
+          name="password"
+          placeholder="Password"
+          required
+          onChange={onChange}
+          values={values[RegisterFormKeys.Password]}
+        />
+        <input
+          type="password"
+          class="rPassword-reg"
+          name="repeat-password"
+          placeholder="Repeat Password"
+          required
+          onChange={onChange}
+          values={values[RegisterFormKeys.PasswordConfirm]}
+        />
+        <button className="sub-button" type="submit">
+          Submit
+        </button>
+        <Link to="/login">
+          <button type="button" class="goToLogin">
+            Have an account
+          </button>
+        </Link>
+      </form>
     </div>
-  
- )
+  );
 }
