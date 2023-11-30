9bc48e068fee74c5d88b7d2fcee611fb6cccc32b
PeykoGeorgiev <peyko92gm@gmail.com>
2023-11-30 17:05:48 +0100

    Migrate from jsonstore to collections

--- client/src/services/storyService.js
+++ client/src/services/storyService.js
@@ -1,13 +1,13 @@
 import * as request from '../lib/request';
 
-const baseUrl = 'http://localhost:3030/jsonstore/stories'
+const baseUrl = 'http://localhost:3030/data/stories'
 
 export const getAll = async () => {
    const result = await request.get(baseUrl);
 
    
 
-   return Object.values(result);
+   return result;
 };
 
 export const getOne = async (storyId) => {
