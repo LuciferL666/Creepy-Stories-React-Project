603c24fdc098cf786dd866afcdc4f7d788a4b683
PeykoGeorgiev <peyko92gm@gmail.com>
2023-11-30 22:13:53 +0100

    fixed comments

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -4,17 +4,17 @@ const baseUrl = 'http://localhost:3030/data/comments';
 
 export const getAll = async (storyId) => {
     const query = new URLSearchParams({
-        where: `storyId="${storyId}"`
-    })
+        where: `storyId="${storyId}"`,
+        load: `owner=_ownerId:users`
+    });
     const result = await request.get(`${baseUrl}?${query}`);
 
     return result;
 };
 
-export const create = async (storyId, username, text) => {
+export const create = async (storyId, text) => {
     const newComment = await request.post(baseUrl, {
         storyId,
-        username,
         text,
     });
 
