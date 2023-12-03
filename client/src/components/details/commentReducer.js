a88a7791faa378d84e60963c53bc1f889735985d
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-03 01:04:16 +0100

    comments reduces

---
+++ client/src/components/details/commentReducer.js
@@ -0,0 +1,12 @@
+ const reducer = (state, action) => {
+    switch (action?.type) {
+        case 'GET_ALL_COMMENTS':
+                return [...action.payload];
+        case 'ADD_COMMENT':
+            return [...state, action.payload]
+        default:
+            return state
+    }
+}
+
+export default reducer;
