dc0ad4391d41c3eb3869526709a4fcd710a2ba9a
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-10 11:37:41 +0100

    Update AuthGuard.jsx

--- client/src/components/guards/AuthGuard.jsx
+++ client/src/components/guards/AuthGuard.jsx
@@ -1,13 +1,13 @@
-import { useContext } from "react"
+import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import AuthContext from "../../contexts/authContext"
+import AuthContext from "../../contexts/authContext";
 
 export default function AuthGuard() {
-    const { isAuthenticated } = useContext(AuthContext)
+  const { isAuthenticated } = useContext(AuthContext);
 
-    if (!isAuthenticated) {
-        return <Navigate to='/login' />;
-    }
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
 
-    return  <Outlet />;
+  return <Outlet />;
 }
