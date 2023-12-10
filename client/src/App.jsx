1f6cbfca986858d9c283cea42a065fab2b718bc4
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-10 11:34:49 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,55 +1,47 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import { AuthProvider } from './contexts/authContext';
-import Path from './paths';
+import { AuthProvider } from "./contexts/authContext";
+import Path from "./paths";
 
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Privacy from './components/Privacy';
-import Err from './components/Err';
-import Register from './components/register/Register';
-import Login from './components/login/Login';
-import CreateStory from './components/create/CreateStory';
-import Catalog from './components/catalog/Catalog';
-import Details from './components/details/Details';
-import Home from './components/home/Home';
-import Logout from './components/logout/Logout';
-import EditStory from './components/story-edit/EditStory';
-import AuthGuard from './components/guards/AuthGuard';
-
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Privacy from "./components/Privacy";
+import Err from "./components/Err";
+import Register from "./components/register/Register";
+import Login from "./components/login/Login";
+import CreateStory from "./components/create/CreateStory";
+import Catalog from "./components/catalog/Catalog";
+import Details from "./components/details/Details";
+import Home from "./components/home/Home";
+import Logout from "./components/logout/Logout";
+import EditStory from "./components/story-edit/EditStory";
+import AuthGuard from "./components/guards/AuthGuard";
 
 function App() {
-
-
   return (
-
     <AuthProvider>
-    <div id='box'>
-    
-<Header />
-<Routes>  
-<Route path={Path.Home} element={ <Home/> }/>
-<Route path='*' element={ <Err />}/>
-<Route path='/privacy' element={ <Privacy /> }/>
-<Route path='/register' element={ <Register /> }/>
-<Route path='/login' element={ <Login />}/>
-<Route path='/catalog' element={ <Catalog />}/>
-<Route path='/story/:storyId/details' element={ <Details />}/>
+      <div id="box">
+        <Header />
+        <Routes>
+          <Route path={Path.Home} element={<Home />} />
+          <Route path="*" element={<Err />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/story/:storyId/details" element={<Details />} />
 
-<Route element={<AuthGuard />}>
-<Route path='/create' element={<CreateStory />}/>
-<Route path={Path.StoryEdit} element={ <EditStory />}/>
-<Route path={Path.Logout} element={ <Logout />} />
-
-</Route>
-
-</Routes>
-<Footer />  
-
-  </div>
-  </AuthProvider>
-  )
+          <Route element={<AuthGuard />}>
+            <Route path="/create" element={<CreateStory />} />
+            <Route path={Path.StoryEdit} element={<EditStory />} />
+            <Route path={Path.Logout} element={<Logout />} />
+          </Route>
+        </Routes>
+        <Footer />
+      </div>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
