718bd852aefa6167dbdb621a16f9dc4b77202eaa
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-10 11:37:21 +0100

    Update StoryItem.jsx

--- client/src/components/catalog/story-item/StoryItem.jsx
+++ client/src/components/catalog/story-item/StoryItem.jsx
@@ -1,20 +1,14 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-export default function StoryItem ({
-    _id,
-    title,
-    category,
-    imageUrl,
-    summary
-}){
-    return (
-        <div class="allStories">
-        <div class="allStories-info">
-            <img src={imageUrl} />
-            <h6>{category}</h6>
-            <h2>{title}</h2>
-            <Link to={`/story/${_id}/details`}>Details</Link>
-        </div>
+export default function StoryItem({ _id, title, category, imageUrl, summary }) {
+  return (
+    <div class="allStories">
+      <div class="allStories-info">
+        <img src={imageUrl} />
+        <h6>{category}</h6>
+        <h2>{title}</h2>
+        <Link to={`/story/${_id}/details`}>Details</Link>
+      </div>
     </div>
-    )
+  );
 }
