275f2bc9bbf46758078e41e2849f21ff61477d62
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-10 11:37:52 +0100

    Update LatestStories.jsx

--- client/src/components/home/latest-stories/LatestStories.jsx
+++ client/src/components/home/latest-stories/LatestStories.jsx
@@ -4,23 +4,22 @@ import { pathToUrl } from "../../../utils/pathUtils";
 import { useContext } from "react";
 import AuthContext from "../../../contexts/authContext";
 
-export default function LatestStories ({
-    _id,
-    imageUrl,
-    title,
-    category,
-}) {
-
-    return (
-        <section className="latest-stories">
-        <ul className="storiesLatest">
+export default function LatestStories({ _id, imageUrl, title, category }) {
+  return (
+    <section className="latest-stories">
+      <ul className="storiesLatest">
         <li className="storiesLatest">
-        <img src={imageUrl} alt="Story 1" />
+          <img src={imageUrl} alt="Story 1" />
           <h3>{title}</h3>
           <p>{category}</p>
-          <Link to={pathToUrl(Path.StoryDetails, {storyId: _id})} className="btnDetailsLatest"><button>Details</button></Link>
+          <Link
+            to={pathToUrl(Path.StoryDetails, { storyId: _id })}
+            className="btnDetailsLatest"
+          >
+            <button>Details</button>
+          </Link>
         </li>
       </ul>
-            </section>
-    )
+    </section>
+  );
 }
