45e719d57723226a54b6f86103de87abc3da496a
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-11 23:55:38 +0100

    Update commentService.js

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -1,22 +1,22 @@
-import * as request from '../lib/request';
+import * as request from "../lib/request";
 
-const baseUrl = 'http://localhost:3030/data/comments';
+const baseUrl = "http://localhost:3030/data/comments";
 
 export const getAll = async (storyId) => {
-    const query = new URLSearchParams({
-        where: `storyId="${storyId}"`,
-        load: `owner=_ownerId:users`,
-    });
-    const result = await request.get(`${baseUrl}?${query}`);
+  const query = new URLSearchParams({
+    where: `storyId="${storyId}"`,
+    load: `owner=_ownerId:users`,
+  });
+  const result = await request.get(`${baseUrl}?${query}`);
 
-    return result;
+  return result;
 };
 
 export const create = async (storyId, text) => {
-    const newComment = await request.post(baseUrl, {
-        storyId,
-        text,
-    });
+  const newComment = await request.post(baseUrl, {
+    storyId,
+    text,
+  });
 
-    return newComment;
-}
+  return newComment;
+};
