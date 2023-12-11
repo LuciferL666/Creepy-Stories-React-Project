1ba39a8d5e4a50e3f9ff3a571faa46d4d8731ffb
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-11 23:55:18 +0100

    Update paths.js

--- client/src/paths.js
+++ client/src/paths.js
@@ -1,10 +1,9 @@
 const Path = {
-    Home: '/',
-    Logout: '/logout',
-    StoryEdit: '/story/:storyId/details/edit',
-    StoryDelete: '/story/:storyId/details/delete',
-    StoryDetails: '/story/:storyId/details',
-
-}
+  Home: "/",
+  Logout: "/logout",
+  StoryEdit: "/story/:storyId/details/edit",
+  StoryDelete: "/story/:storyId/details/delete",
+  StoryDetails: "/story/:storyId/details",
+};
 
 export default Path;
