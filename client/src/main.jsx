a01c17fda7e71ee02c9f159d254d41869b8ac991
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-11 23:55:22 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-    </BrowserRouter>
-)
+  </BrowserRouter>
+);
