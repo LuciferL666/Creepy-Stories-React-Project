e886950157fbe34a043202fdf1cf8afac85ef30b
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-11 23:55:28 +0100

    Update request.js

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -1,47 +1,46 @@
-const buildOptions =  (data) => {
-    const options = {};
+const buildOptions = (data) => {
+  const options = {};
 
-    if(data) {
-        options.body = JSON.stringify(data);
-        options.headers = {
-            'content-type': 'application/json'
-        };
-    }
+  if (data) {
+    options.body = JSON.stringify(data);
+    options.headers = {
+      "content-type": "application/json",
+    };
+  }
 
-    const token = localStorage.getItem('accessToken');
+  const token = localStorage.getItem("accessToken");
 
-    if (token) {
-        options.headers = {
-            ...options.headers,
-            'X-Authorization': token
-        };
-    }
+  if (token) {
+    options.headers = {
+      ...options.headers,
+      "X-Authorization": token,
+    };
+  }
 
-    return options
-}
-
-
-const request = async (method, url, data) => {
-    const response = await fetch(url, {
-        ...buildOptions(data),
-        method,
-    });
-
-    if(response.status === 204) {
-        return {};
-    }
-
-    const result = await response.json();
-
-    if (!response.ok) {
-        throw result;
-    }
-
-    return result;
+  return options;
 };
 
-export const get = request.bind(null, 'GET');
-export const post = request.bind(null, 'POST');
-export const put = request.bind(null, 'PUT');
-export const del = request.bind(null, 'DELETE');
-export const patch = request.bind(null, 'PATCH');
+const request = async (method, url, data) => {
+  const response = await fetch(url, {
+    ...buildOptions(data),
+    method,
+  });
+
+  if (response.status === 204) {
+    return {};
+  }
+
+  const result = await response.json();
+
+  if (!response.ok) {
+    throw result;
+  }
+
+  return result;
+};
+
+export const get = request.bind(null, "GET");
+export const post = request.bind(null, "POST");
+export const put = request.bind(null, "PUT");
+export const del = request.bind(null, "DELETE");
+export const patch = request.bind(null, "PATCH");
