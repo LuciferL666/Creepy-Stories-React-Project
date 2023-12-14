5213b8f2602fb5b59b06fbab284b766c0ac438ed
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-14 23:52:24 +0100

    Update Logout.jsx

--- client/src/components/logout/Logout.jsx
+++ client/src/components/logout/Logout.jsx
@@ -1,25 +1,26 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect } from "react";
 
-import * as authService from '../../services/authService'
-import { useNavigate } from "react-router-dom"
+import * as authService from "../../services/authService";
+import { useNavigate } from "react-router-dom";
 import AuthContext from "../../contexts/authContext";
 import Path from "../../paths";
 
-export default function Logout () {
-    const navigate = useNavigate();
-    const {logoutHandler} = useContext(AuthContext);
+export default function Logout() {
+  const navigate = useNavigate();
+  const { logoutHandler } = useContext(AuthContext);
 
-    useEffect(() => {
-        authService.logout()
-        .then(() => {
-            logoutHandler();
-            navigate(Path.Home);
-        })
-        .catch(() => {
-            logoutHandler();
-            navigate(Path.Home)
-        });
-    }, [])
+  useEffect(() => {
+    authService
+      .logout()
+      .then(() => {
+        logoutHandler();
+        navigate(Path.Home);
+      })
+      .catch(() => {
+        logoutHandler();
+        navigate(Path.Home);
+      });
+  }, []);
 
-    return null
+  return null;
 }
