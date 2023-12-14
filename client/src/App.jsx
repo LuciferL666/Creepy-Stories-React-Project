f854eb45341e0a97797eac96b0a79ab987d59d6c
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-14 23:51:42 +0100

    Add toastify

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import { AuthProvider } from "./contexts/authContext";
 import Path from "./paths";
 
@@ -22,6 +25,7 @@ function App() {
   return (
     <AuthProvider>
       <div id="box">
+      <ToastContainer />
         <Header />
         <Routes>
           <Route path={Path.Home} element={<Home />} />
