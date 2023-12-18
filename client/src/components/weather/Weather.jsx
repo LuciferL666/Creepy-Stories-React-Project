b3277de4c8b5008505f29ba6ae15ea650911fbb0
PeykoGeorgiev <peyko92gm@gmail.com>
2023-12-18 22:10:25 +0100

    Create Weather.jsx

---
+++ client/src/components/weather/Weather.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import * as weatherService from "../../services/weatherServices"
+import "../../../public/styles/30_pages/weather.css";
+
+export default function Weather(){
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [isWeatherOpen, setIsWeatherOpen] = useState(false);
+
+  const updateCity = async (city) => {
+    const cityInfo = await weatherService.getCity(city);
+    const weatherInfo = await weatherService.getWeather(cityInfo.Key);
+
+    setWeatherData({
+      cityInfo,
+      weatherInfo,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateCity(city);
+    setCity("");
+  };
+
+  const toggleWeather = () => {
+    setIsWeatherOpen((prev) => !prev);
+  };
+
+  return (
+    <section className={`weather ${isWeatherOpen ? "weather-open" : ""}`}>
+      <button className="open-close" onClick={toggleWeather}>
+        <p>&#9730;</p>
+      </button>
+      <div className="weather-card">
+        <form className="city-form" onSubmit={handleSubmit}>
+          <div className="location-input">
+            <input
+              type="text"
+              name="city"
+              placeholder="Enter location"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </div>
+        </form>
+        <div className="city">
+          {" "}
+          {weatherData?.cityInfo?.LocalizedName || "City"}
+        </div>
+        <div className="weather-img">
+          {weatherData?.weatherInfo?.WeatherIcon ? (
+            <img
+              src={`../../../public/images/weather/${weatherData?.weatherInfo?.WeatherIcon}.svg`}
+              alt="weather-img"
+            />
+          ) : (
+            <img
+              src="../../../public/images/weather/default.png"
+              alt="default-weather-img"
+            />
+          )}
+        </div>
+        <div className="temperature">
+          {weatherData?.weatherInfo?.Temperature?.Metric?.Value ||
+            "Temperature"}
+          °C
+        </div>
+        <div className="condition">
+          {weatherData?.weatherInfo?.WeatherText || "Weather condition"}
+        </div>
+      </div>
+    </section>
+  );
+}
